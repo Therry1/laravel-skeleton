dbd4f4b8c84bba9b7c0dc0e0bfbc54b4810feb36
Therry1 <139223690+Therry1@users.noreply.github.com>
2025-11-21 11:02:14 +0100

    retrait du middleware de de control d'acces

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'check.perission' => \App\Http\Middleware\PermissionMiddleware::class,
-        ]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
